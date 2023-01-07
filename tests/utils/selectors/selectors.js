b30256c065d95c78cbab4937acc7c3b176e1ae7a
timeades <tim.eades@mac.com>
2023-01-07 14:29:31 +0000

    add more support files

---
+++ tests/utils/selectors/selectors.js
@@ -0,0 +1,12 @@
+export const HomepageComponent {
+    searchIcon = '[href*="/en-DE/search"]', //temporary fix
+    searchInput = '[data-test-id="SEARCH_INPUT"]',
+    daznIcon = '[data-test-id="LOGO"]',
+    errorModal = '[data-test-id="TOAST_CONTAINER"]',
+    categoryTitle = '(//section//h2[text()="All Sports"])//following-sibling::div/div/div/div/div/article/a/footer/div/h4',
+    categoryRail = '(//section//h2[text()="All Sports"])',
+    categoryBannerTitle = '[data-test-id="BANNER_TITLE"]',
+    categoryBannerSubtitle = '[data-test-id="BANNER_DESCRIPTION"]',
+    categoryBannerImage = '[data-test-id="BANNER_IMAGE"]',
+    favouriteButton = '[data-test-id="FAVOURITE_TITLE"]'
+}
